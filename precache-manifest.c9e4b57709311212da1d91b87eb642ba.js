f32a3f2f50460a6ef7a9db72e696ecdcd1138136
alexaergun <alexaergun@gmail.com>
2021-04-09 11:00:37 -0600

    Updates

---
+++ precache-manifest.c9e4b57709311212da1d91b87eb642ba.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a02b4470bd8f16fe672e7500b1cf32c7",
+    "url": "/myreactportfolio/index.html"
+  },
+  {
+    "revision": "216ed66e030c9e49acd3",
+    "url": "/myreactportfolio/static/css/2.8393c96e.chunk.css"
+  },
+  {
+    "revision": "6fc29fe0176228a91520",
+    "url": "/myreactportfolio/static/css/main.93fbf6cf.chunk.css"
+  },
+  {
+    "revision": "216ed66e030c9e49acd3",
+    "url": "/myreactportfolio/static/js/2.aac2f722.chunk.js"
+  },
+  {
+    "revision": "29a5a22dd5ee304ddb57580c6c358d04",
+    "url": "/myreactportfolio/static/js/2.aac2f722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fc29fe0176228a91520",
+    "url": "/myreactportfolio/static/js/main.f40561e8.chunk.js"
+  },
+  {
+    "revision": "3ae8c3956a9c1d9cf4ad",
+    "url": "/myreactportfolio/static/js/runtime-main.27fc3a82.js"
+  },
+  {
+    "revision": "1a6386f927278bed74c87c9841452aad",
+    "url": "/myreactportfolio/static/media/DayPlanner1.1a6386f9.png"
+  },
+  {
+    "revision": "2b4c617a95d2d5bb00e980a76dc0ecbc",
+    "url": "/myreactportfolio/static/media/GameBuzz.2b4c617a.png"
+  },
+  {
+    "revision": "11e956222ede1fedf8a63875b89cc825",
+    "url": "/myreactportfolio/static/media/HobbyDrop.11e95622.png"
+  },
+  {
+    "revision": "b9cf8cb77722189d19a0ca8e3612f93e",
+    "url": "/myreactportfolio/static/media/TopBoardGames.b9cf8cb7.png"
+  },
+  {
+    "revision": "7cfbed606639a4e7e74fe9710728a862",
+    "url": "/myreactportfolio/static/media/WeatherDash1.7cfbed60.png"
+  },
+  {
+    "revision": "1b2da0f2fdde0add9e08cb96e6d05559",
+    "url": "/myreactportfolio/static/media/cloud-backend.1b2da0f2.svg"
+  },
+  {
+    "revision": "a49fe13b8f9fedb0d94a8ca9387f030f",
+    "url": "/myreactportfolio/static/media/cloud-databases.a49fe13b.svg"
+  },
+  {
+    "revision": "9105a6574c2b90ac9a19406b9fe2ab51",
+    "url": "/myreactportfolio/static/media/cloud-frontend.9105a657.svg"
+  },
+  {
+    "revision": "3290a95597577f67b994a290a10fcb63",
+    "url": "/myreactportfolio/static/media/cloud-services.3290a955.svg"
+  },
+  {
+    "revision": "7447a07fb9a2554076b477be54b45c4c",
+    "url": "/myreactportfolio/static/media/github.7447a07f.svg"
+  },
+  {
+    "revision": "feb2f8ec3e6dfc08c6bf4a7a2ab9f497",
+    "url": "/myreactportfolio/static/media/hummingbird.feb2f8ec.svg"
+  },
+  {
+    "revision": "55be353564668d29eb5469ea6cb7506d",
+    "url": "/myreactportfolio/static/media/linkedin.55be3535.svg"
+  },
+  {
+    "revision": "783e7fd97e70ea74774a0be1af03798b",
+    "url": "/myreactportfolio/static/media/sun.783e7fd9.svg"
+  }
+]);
