1024f84fe2451cf313df617281be49df17bab239
alexaergun <alexaergun@gmail.com>
2021-05-02 18:30:32 -0600

    making updates to about me, name animation, and added resume to portfolio

--- src/components/About.js
+++ src/components/About.js
@@ -6,10 +6,10 @@ const About = (props) => (
       <div className="content-container">
         <div className="about-all">
           <div className="section-heading--about">
-            <h1>About Me</h1>
+            <h1>Hello There!</h1>
           </div>
           <h3>
-            <em>Oh hello there! I'm <strong>Alexandria Atwell</strong></em>
+            <em>I'm Alexandria Atwell</em>
           </h3>
           <p>
           Passionate and creative baker turned web developer. Bringing forth a motivated attitude and a variety of powerful skills; JS, React, CSS, HTML, SQL, MongoDB, Express, 
@@ -20,7 +20,7 @@ const About = (props) => (
             <em>Where I'm From</em>
           </h3>
           <p>
-          Colorado born, I have always loved <strong>GAMING</strong>. I dreamt of being a game designer. I hold multiple degree's and certificates; a Bachelor Degree in Graphic Arts and an Associates degree as a Culinary/Pastry Chef. I am also a certified Bartender who also recently completed my Certification as Full Stack Web Developer.
+          Colorado born, I have always loved <strong>Software Development and Video Games</strong>. I hold multiple degree's and certificate's; a Bachelor Degree in Graphic Arts and an Associates degree as a Culinary/Pastry Chef. I am also a certified Bartender who also recently completed my Certification as Full Stack Web Developer.
           Technology is ever changing and I want to be a part of the technology evolution. I define myself by influencing the growth of where technology will be heading. I am Self-motivated, results oriented with a proven track record of working collaboratively with team members to achieve goals.
           Experienced in both retail and culinary settings, and adept at effectively managing operations. I have excellent time management skills combined with a superior knowledge of the customer service industry. 
           </p>
@@ -28,8 +28,8 @@ const About = (props) => (
             <em>Where I'm Headed</em>
           </h3>
           <p>
-            How many folks can agree that the most satisfying part of being a Web Dev, is being able to explore your wild and creative streak? I <strong>LOVE</strong> the chaotic nature of coding! The satisfying challenge when your code breaks, spending several hours upon end; when finally, a <em>breakthrough</em>!
-            finally it works. I live for that, building more than an app or website, creating the <strong>FUTURE!!</strong>
+          I live and breathe web development. How many folks can agree that the most satisfying part of being a Web Dev, is being able to explore your wild and creative streak? I <strong>LOVE</strong> the chaotic nature of coding! The gratifying challenge when your code breaks, spending several hours upon end; when finally,
+          a <strong><em>breakthrough!</em></strong> Let's discover our tech filled adventure, building more than an app or website, creating the...<strong>FUTURE!!</strong>
           </p>
         </div>
       </div>
